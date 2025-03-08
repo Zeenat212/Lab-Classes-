339ef416006f9633e9bd19892e0b7a0c7c403299
Zeenat212 <24ntucsfl1219@student.ntu.edu.pk>
2025-03-08 00:19:17 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trip trip = new Trip("New York", 300, 50, 15);
+            trip.ShowTrip();
+
+        }
+    }
+}
---
+++ Trip.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+    internal class Trip
+    {
+        public string Destination;
+        public double Distance;
+        public double GasCost;
+        public double Gallons;
+
+        public Trip(string destination, double distance, double gasCost, double gallons)
+        {
+            Destination = destination;
+            Distance = distance;
+            GasCost = gasCost;
+            Gallons = gallons;
+        }
+
+        public double MilesPerGallon()
+        {
+            return Distance / Gallons;
+        }
+
+        public double CostPerMile()
+        {
+            return GasCost / Distance;
+        }
+
+        public void ShowTrip()
+        {
+            Console.WriteLine("Trip to " + Destination);
+            Console.WriteLine("Miles per gallon: " + MilesPerGallon());
+            Console.WriteLine("Cost per mile: " + CostPerMile());
+        }
+
+    }
+}
