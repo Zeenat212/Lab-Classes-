a1d0859bb2ffecc6da1578738f54dd995bd62974
Zeenat212 <24ntucsfl1219@student.ntu.edu.pk>
2025-03-08 00:24:33 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,30 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classes4
+namespace Classes2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Book book = new Book();
-            book.Input();
-            book.PrintBook();
-            Console.WriteLine("\t\t<--------------|Search a Book|-------------->");
+            Fraction fraction = new Fraction(10, 100);
+            fraction.ShowFraction();
 
-            Console.WriteLine("Enter The Book Title Do you want to Search: ");
-            string book1 = Console.ReadLine();
-            for (int i = 0; i < book.Title.Length; i++)
-            {
-                if (book.Title[i] == book1)
-                {
-                    book.Search(book1);
-                }
-            }
+
         }
-
-
-
     }
 }
-   
